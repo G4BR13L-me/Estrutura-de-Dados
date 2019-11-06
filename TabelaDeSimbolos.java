82ca17c61ec7d75258752fa3f868f65c57f1b808
Gabriel Menezes Moreira <52969347+G4BR13L-me@users.noreply.github.com>
2019-11-06 13:36:49 -0300

    Update TabelaDeSimbolos.java

--- TabelaDeSimbolos.java
+++ TabelaDeSimbolos.java
@@ -1,26 +1,63 @@
 package br.ufpi.ed;
 
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
+
 
 import javax.swing.JOptionPane;
 
 public class TabelaDeSimbolos {
+	private List<Integer> key;
+	private List<Integer> value;
 	
-	public static Set<Integer> criaNumeros(int num) {
-		Random rd = new Random();
-		Set<Integer> numeros = new HashSet<Integer>();
-		int cont=0;
-		while(cont<num*128) {
-			if(numeros.add(rd.nextInt()))
-				cont++;
-		}
-		return numeros;
+	public TabelaDeSimbolos() {
+		this.key = new LinkedList<Integer>();
+		this.value = new LinkedList<Integer>();
 	}
 	
-	public static boolean buscaBinaria(String a,int indice, String[] array) {
+	public void adicionar(Integer key,Integer value) {
+		int index = this.key.indexOf(key);
+		if(index!=-1) {
+			this.key.add(key);
+			this.value.add(value);
+		}else {
+			this.value.add(index, value);
+			this.value.remove(index+1);
+		}
+	}
+	
+	public boolean remove(Integer key) {
+		int index = this.key.indexOf(key);
+		if(index!=-1) {
+			this.key.remove(index);
+			this.value.remove(index);
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean buscaBinaria(Integer key) {
+//		array[indice]="";
+//		
+//		do{
+//			meio=(ini+fim)/2;
+//			if(a.compareTo(array[meio])<0)
+//				fim=meio-1;
+//			else if(a.compareTo(array[meio])>0)
+//				ini=meio+1;
+//			else 
+//				flag=1;
+//		}while(flag!=1&&ini<fim);
+//		
+//		array[indice]=a;
+//		
+//		if(flag==1)
+//			return true;
+//		return false;
+	}
+	
+	public static boolean buscaSequencial(Integer a) {
 		int ini = 0, fim = array.length-1;
 		int meio,flag=0;
 		array[indice]="";
@@ -42,26 +79,84 @@ public class TabelaDeSimbolos {
 		return false;
 	}
 	
-	
-	public static void main(String[] args) {
-		
-		do {
-			int escolha = Integer.parseInt(JOptionPane.showInputDialog("MENU:\n1 - Fazer experimento\n0 - Sair"));
-			if(escolha!=1)
-				break;
-			int num = Integer.parseInt(JOptionPane.showInputDialog("Informe o numero de chaves desejado:"));
-			int busca = Integer.parseInt(JOptionPane.showInputDialog("Busca:\n1 - Sequencial\n2 - Bin�ria"));
-			switch (busca) {
-			case 1:
-				int[] numeros = criaNumeros
-				break;
-			case 2:
-				
-				break;	
-			default:
-				break;
-			}
-			
-		}while(true);
+	@Override
+	public String toString() {
+		String str="";
+		for (Integer n : key) {
+			str+=n+" - ";
+		}
+		return str;
 	}
+	
+	
+	
+	
+	
+	
+//	public static List<Integer> criaNumeros(int num,int rep) {
+//		Random rd = new Random();
+//		List<Integer> numeros = new LinkedList<Integer>();
+//		int cont=0;
+//		double valor=num*Math.pow(2, rep);
+//		while(cont<valor-1) {
+//			Integer numero = rd.nextInt();
+//			if(numeros.contains(numero)) {
+//				numeros.add(numero);
+//			}else {
+//				numeros.add(numero);
+//				cont++;
+//			}
+//		}
+//		return numeros;
+//	}
+//	
+//	public static boolean buscaBinaria(Integer a,  array) {
+//		int ini = 0, fim = array.length-1;
+//		int meio,flag=0;
+//		array[indice]="";
+//		
+//		do{
+//			meio=(ini+fim)/2;
+//			if(a.compareTo(array[meio])<0)
+//				fim=meio-1;
+//			else if(a.compareTo(array[meio])>0)
+//				ini=meio+1;
+//			else 
+//				flag=1;
+//		}while(flag!=1&&ini<fim);
+//		
+//		array[indice]=a;
+//		
+//		if(flag==1)
+//			return true;
+//		return false;
+//	}
+//	
+//	
+//	
+//	public static void main(String[] args) {
+//		
+//		do {
+//			int escolha = Integer.parseInt(JOptionPane.showInputDialog("MENU:\n1 - Fazer experimento\n0 - Sair"));
+//			if(escolha!=1)
+//				break;
+//			int num = Integer.parseInt(JOptionPane.showInputDialog("Informe o numero de chaves desejado:"));
+//			int repeticoes = Integer.parseInt(JOptionPane.showInputDialog("Quantas repetições você deseja realizar?"));
+//			int busca = Integer.parseInt(JOptionPane.showInputDialog("Busca:\n1 - Sequencial\n2 - Binária"));
+//			switch (busca) {
+//			case 1:
+//				List<Integer> numeros = criaNumeros(num,repeticoes);
+//				
+//				
+//				break;
+//			case 2:
+//				
+//				
+//				break;	
+//			default:
+//				break;
+//			}
+//			
+//		}while(true);
+//	}
 }
